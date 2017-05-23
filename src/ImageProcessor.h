e67988dae8357f55ea0df74fac392a67469c20a4
edkarlsson <Erikdkarlsson8@gmail.com>
2017-05-23 10:25:31 -0700

    Working on IP.h

--- src/ImageProcessor.h
+++ src/ImageProcessor.h
@@ -5,9 +5,34 @@
 #ifndef FIRE_EMBLEM_AI_IMAGEPROCESSOR_H
 #define FIRE_EMBLEM_AI_IMAGEPROCESSOR_H
 
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <boost/filesystem.hpp>
+
 class ImageProcessor
 {
+    public:
+        ImageProcessor(std::string imageName);
 
+        cv::Mat cropNameFromImage();
+        cv::Mat cropHealthFromImage();
+        cv::Mat cropRemainingHealthFromImage();
+        cv::Mat cropAtkFromImage();
+        cv::Mat cropDefFromImage();
+        cv::Mat cropResFromImage();
+        cv::Mat cropSpdFromImage();
+        cv::Mat cropWeaponFromImage();
+        cv::Mat cropAssistFromImage();
+        cv::Mat cropSpecialFromImage();
+        cv::Mat cropSlotAFromImage();
+        cv::Mat cropSlotBFromImage();
+        cv::Mat cropSlotCFromImage();
+        cv::Mat cropSealFromImage();
+
+    private:
+        std::string imageName;
+        cv::Mat     image;
 };
 
 #endif //FIRE_EMBLEM_AI_IMAGEPROCESSOR_H
