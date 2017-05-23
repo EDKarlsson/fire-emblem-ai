f93fd09dd6ecc34fb3e1edaf6a3be9d729f00412
edkarlsson <Erikdkarlsson8@gmail.com>
2017-05-23 09:02:57 -0700

    Committing images. Got the initial crop to work correctly. Next is to design a class that will crop all the pieces of information from the image.

---
+++ ImageProcessor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erik Karlsson on 5/23/17.
+//
+
+#include "ImageProcessor.h"
---
+++ ImageProcessor.h
@@ -0,0 +1,13 @@
+//
+// Created by Erik Karlsson on 5/23/17.
+//
+
+#ifndef FIRE_EMBLEM_AI_IMAGEPROCESSOR_H
+#define FIRE_EMBLEM_AI_IMAGEPROCESSOR_H
+
+class ImageProcessor
+{
+
+};
+
+#endif //FIRE_EMBLEM_AI_IMAGEPROCESSOR_H
